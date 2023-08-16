d8d6b4ba3a6f67935a336cf54623c1002b3f4cb2
AtaisiFriday <fridayataisi@gmail.com>
2023-08-16 12:53:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * Description
+ * Return: always (0)
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		_putchar(lc);
+	}
+		_putchar('\n');
+	return (0);
+
+}
